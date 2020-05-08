32077e1d81ac51caf528b6e1120070a5e2f6ffd9
Maria <m.titova95@inbox.ru>
2020-05-08 18:53:58 +0300

    finished styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,51 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  length = 12;
+  includeLetters = true;
+  includeNumbers = true;
+  includeSymbols = true;
   password = '';
   onButtonClick() {
-    this.password = 'Vaska';
+    const numbers = '1234567890';
+    const letters = 'abcdefghijklmnopqrstuvwyzABCDEFGHIJKLMNOPQRSTUVWYZ';
+    const symbols = '!@#$%^&*()-+{}[]';
+
+    let validChars = '';
+    if (this.includeLetters) {
+      validChars += letters;
+    }
+    if (this.includeSymbols) {
+      validChars += symbols;
+    }
+    if (this.includeNumbers) {
+      validChars += numbers;
+    }
+    let generatedPassword = '';
+    for (let i = 0; i < this.length; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+    this.password = generatedPassword;
+  }
+
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onChangeUseNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  onChangeUseSymbols() {
+    this.includeSymbols = !this.includeSymbols;
+  }
+
+  onChangeLength(event) {
+    // tslint:disable-next-line:radix
+    const parsedValue: number = parseInt(event.target.value);
+    if (!isNaN(parsedValue)) {
+      this.length = parsedValue;
+    }
   }
 }
